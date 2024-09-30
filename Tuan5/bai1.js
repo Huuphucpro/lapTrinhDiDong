d47f15bb6ef20fd93e0d32f0e566f17db61d7df0
HuuphucPro <tranhuuphuc599@gmail.com>
2024-09-30 13:22:13 +0700

    bo sung tuan5

---
+++ Tuan5/bai1.js
@@ -0,0 +1,28 @@
+//JavaScript Fundamentals – Part 1
+
+//Coding Challenge #1
+console.log('Bai1');
+
+//Data 1:
+let weightMark = 78;
+let heightMark = 1.69;
+let BMIMark = weightMark / (heightMark ** 2);
+
+let weightJohn = 92;
+let heightJohn = 1.95;
+let BMIJohn = weightJohn / (heightJohn ** 2);
+
+markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+
+//Data 2:
+weightMark = 95;
+heightMark = 1.88;
+BMIMark = weightMark / (heightMark ** 2);
+
+weightJohn = 85;
+heightJohn = 1.76;
+BMIJohn = weightJohn / (heightJohn ** 2);
+
+markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
